5b9c6c757cdf7f95ac89ffb947e2ca7925ed783e
Ishmeet Kaur <ishmeetkaur364@gmail.com>
2025-08-22 16:08:58 +0000

    heroImage

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,45 @@
 import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button"
+import { features } from "@/data/features";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 export default function Home() {
   return (
   <div>
     <div className="grid-background"></div>
 
     <HeroSection/>
+    <section>
+      <div>
+        <h2>Powerful Features for Your Career Growth</h2>
+        <div>{features.map((feature,index) => {
+          return (
+            <Card>
+             <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+               <CardDescription>Card Description</CardDescription>
+                <CardAction>Card Action</CardAction>
+             </CardHeader>
+             <CardContent>
+              <p>Card Content</p>
+             </CardContent>
+             <CardFooter>
+             <p>Card Footer</p>
+             </CardFooter>
+           </Card>
+          )
+        })}
+        </div>
+      </div>
+    </section>
   </div>
           
    
--- components/hero.jsx
+++ components/hero.jsx
@@ -1,9 +1,32 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 
 const HeroSection =() => {
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
     return(
         <section className="w-full pt-36 md:pt-48 pb-10">
             <div className="space-y-6 text-center">
@@ -14,32 +37,32 @@ const HeroSection =() => {
                         <br />
                         Professional Success
                     </h1>
-                    <p>
+                    <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
                         Advacne your career with personalized guidance , interview prep and 
                         AI-powered tools for job success.
                     </p>
                 </div>
 
-                <div>
+                <div className="flex justify-center space-x-4">
                     <Link href="/dashboard">
-                       <Button size="lg" className="px-8">
-                         Get Started
+                       <Button size="lg" className="px-8 bg-white text-black">
+                         Watch Demo
                         </Button>
                     </Link>
                     <Link href="https://www.youtube.com/">
-                      <Button size="lg" className="px-8" variant="outline">
+                      <Button size="lg" className="px-8" variant="outline" >
                          Get Started
                         </Button>
                     </Link>
                 </div>
 
-                <div>
-                    <div>
+                <div className="hero-image-wrapper mt-5 md:mt-0">
+                    <div ref={imageRef} className="hero-image">
                         <Image
                         src={"/banner.jpeg"}
-                        width={1280}
+                        width={1200}
                         height={720}
-                        alt="Banner Sensai"
+                        alt="Dashboard Preview"
                         className="rounded-lg shadow-2xl border mx-auto"
                         priority
                         />
